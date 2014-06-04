9d0b522f3e1f14a9bdaf8b9b28ab3d9c591ca312
Mathieu Lecarme <mlecarme@bearstech.com>
2014-06-04 16:14:43 +0200

    Spamhaus.

--- main.go
+++ main.go
@@ -5,10 +5,35 @@ import (
 	"fmt"
 	"github.com/nranchev/go-libGeoIP"
 	"io"
+	"net"
 	"os"
 	"regexp"
+	"strings"
 	"time"
 )
+
+func rbl(ip string) (status string) {
+	blocks := strings.Split(ip, ".")
+	name := fmt.Sprintf("%s.%s.%s.%s.zen.spamhaus.org", blocks[3], blocks[2], blocks[1], blocks[0])
+	r, err := net.LookupHost(name)
+	if err != nil {
+		//Optimistic answser
+        return "-"
+	}
+	if r[0] == "127.0.0.10" || r[0] == "127.0.0.11" {
+		return "PBL"
+	}
+	if r[0] == "127.0.0.2" {
+		return "SBL"
+	}
+	if r[0] == "127.0.0.3" {
+		return "CSS"
+	}
+	if r[0] == "127.0.0.4" || r[0] == "127.0.0.5" || r[0] == "127.0.0.6" || r[0] == "127.0.0.7" {
+		return "XBL"
+	}
+	return r[0]
+}
 
 func consolidate(gi *libgeo.GeoIP, count chan string) {
 	scores := make(map[string]int)
@@ -20,7 +45,8 @@ func consolidate(gi *libgeo.GeoIP, count chan string) {
 		case <-c:
 			for ip, n := range scores {
 				loc := gi.GetLocationByIP(ip)
-				fmt.Printf("%s %s #%d\n", loc.CountryCode, ip, n)
+				status := rbl(ip)
+				fmt.Printf("%s %s #%d %s\n", loc.CountryCode, ip, n, status)
 			}
 			scores = make(map[string]int)
 		}
