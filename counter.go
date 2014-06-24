881e95f3b72776f14f793565dc904f448ce28791
Mathieu Lecarme <mlecarme@bearstech.com>
2014-06-24 23:20:29 +0200

    golint suggests more comments.

--- counter.go
+++ counter.go
@@ -4,16 +4,25 @@ import (
 	"hash/fnv"
 )
 
+/*
+Counter tha count distinct stuff
+*/
 type Counter struct {
 	data map[uint32]bool
 }
 
+/*
+NewCounter return a new Counter.
+*/
 func NewCounter() *Counter {
 	c := new(Counter)
 	c.data = make(map[uint32]bool)
 	return c
 }
 
+/*
+Add something to count
+*/
 func (c Counter) Add(element string) {
 	hash := fnv.New32()
 	hash.Write([]byte(element))
@@ -21,6 +30,9 @@ func (c Counter) Add(element string) {
 	c.data[h] = true
 }
 
+/*
+Size of the set
+*/
 func (c Counter) Size() (size int) {
 	return len(c.data)
 }
