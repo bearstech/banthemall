2b3aa3875b874f99c4880005ef71f10cf5bf5af2
Mathieu Lecarme <mlecarme@bearstech.com>
2014-07-13 00:44:49 +0200

    Choose your timestamp.

--- src/banthemall/capped/capped.go
+++ src/banthemall/capped/capped.go
@@ -1,6 +1,7 @@
 package capped
 
 import (
+	"errors"
 	"math"
 	"sync"
 	"time"
@@ -11,20 +12,64 @@ type CappedMetrics struct {
 	values []float64
 	mutex  sync.Mutex
 	start  time.Time
+	end    time.Time
 	step   time.Duration
+	size   int
 }
 
 func NewCappedMetrics(size int, start time.Time, step time.Duration) *CappedMetrics {
 	c := &CappedMetrics{
 		zero:   0,
 		values: make([]float64, size),
-		start:  start,
+		start:  start.Truncate(step),
 		step:   step,
 	}
+	c.end = c.start
 	for i := 0; i < size; i++ {
 		c.values[i] = math.NaN()
 	}
 	return c
+}
+
+func (c *CappedMetrics) Update() {
+	n := time.Now().Truncate(c.step)
+	if n.Equal(c.end) {
+		return
+	}
+	diff := n.Sub(c.end)
+	for i := int64(0); i < int64(diff); i += int64(c.step) {
+		c.AddNothing()
+	}
+}
+
+func (c *CappedMetrics) Metric(t time.Time, value float64) error {
+	n := time.Now()
+	t = t.Truncate(c.step)
+	if t.After(n) {
+		return errors.New("No futur")
+	}
+	if t.Before(c.start) {
+		// Too late
+		return errors.New("Too old")
+	}
+	if t.After(c.end) { // oups, we are late
+		c.Update()
+	}
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	diff := t.Sub(c.end)
+	poz := int(int64(diff)/int64(c.step)) + c.zero
+	l := len(c.values)
+	if poz > l {
+		poz -= l
+	}
+	if poz < 0 {
+		poz += l
+	}
+	c.values[poz] = value
+
+	return nil
 }
 
 func (c *CappedMetrics) Add(value float64) {
@@ -33,10 +78,15 @@ func (c *CappedMetrics) Add(value float64) {
 
 	c.values[c.zero] = value
 	c.zero++
+	c.end = c.end.Add(c.step)
+	if c.size < len(c.values) {
+		c.size++
+	} else {
+		c.start.Add(c.step)
+	}
 	if c.zero >= len(c.values) {
 		c.zero = 0
 	}
-	c.start.Add(c.step)
 }
 
 func (c *CappedMetrics) AddNothing() {
@@ -44,8 +94,19 @@ func (c *CappedMetrics) AddNothing() {
 }
 
 type Values struct {
-	ts    time.Time
-	value float64
+	Time  time.Time
+	Value float64
+}
+
+func (c *CappedMetrics) modulo(n int) int {
+	l := len(c.values)
+	if n >= l {
+		return c.modulo(n - l)
+	}
+	if n < 0 {
+		return c.modulo(n + l)
+	}
+	return n
 }
 
 func (c *CappedMetrics) Values() []*Values {
@@ -63,14 +124,15 @@ func (c *CappedMetrics) Values() []*Values {
 	}
 	values := make([]*Values, len(c.values)-empty)
 	ts := c.start
-	i := 0
-	for _, v := range c.values {
+	j := 0
+	for i := 0; i < len(c.values); i++ {
+		v := c.values[c.modulo(c.zero-len(c.values)+1+i)]
+		ts = ts.Add(c.step)
 		if !math.IsNaN(v) {
 			val := Values{ts, v}
-			values[i] = &val
-			i++
+			values[j] = &val
+			j++
 		}
-		ts.Add(c.step)
 	}
 	return values
 }
--- src/banthemall/capped/capped_test.go
+++ src/banthemall/capped/capped_test.go
@@ -20,6 +20,23 @@ func TestCapped(t *testing.T) {
 	assert.True(t, math.IsNaN(c.values[1]))
 	values := c.Values()
 	assert.Equal(t, 2, len(values))
-	assert.Equal(t, 3, values[0].value)
-	assert.Equal(t, 2, values[1].value)
+	assert.Equal(t, 3, values[0].Value)
+	assert.Equal(t, 2, values[1].Value)
+}
+
+func TestMetric(t *testing.T) {
+	n := time.Now()
+	c := NewCappedMetrics(3, n.Add(-3*time.Minute), time.Minute)
+	err := c.Metric(n.Add(-1*time.Minute), 37)
+	assert.Equal(t, nil, err)
+	err = c.Metric(n, 5)
+	assert.Equal(t, nil, err)
+	values := c.Values()
+	assert.Equal(t, 2, len(values))
+	assert.True(t, values[1].Time.After(values[0].Time))
+	assert.Equal(t, 37, values[0].Value)
+	assert.Equal(t, 5, values[1].Value)
+	nn := n.Truncate(time.Minute)
+	assert.Equal(t, nn.Add(-1*time.Minute), values[0].Time)
+	assert.Equal(t, nn, values[1].Time)
 }
